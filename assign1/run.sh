a77c5d72c868dfa8fdd002cd8514120aeb5fed51
Sola Lhim <131292558+pooloom69@users.noreply.github.com>
2025-09-02 22:32:01 -0700

    Update run.sh

--- assign1/run.sh
+++ assign1/run.sh
@@ -6,7 +6,7 @@
 
 set -e
 echo "Remove old executable files if there are any"
-rm *.out
+rm -f *.out *.o
 
 echo "Assenble the X86 file deliver.asm"
 nasm -f elf64 -o deliver.o deliver.asm
@@ -15,10 +15,7 @@ echo "Compile the C++ file main.cpp"
 g++ -c -m64 -Wall -no-pie -std=c++17 -o main.o main.cpp
 
 echo "Link the two 'o' files main.o deliver.o"
-g++ -m64 -no-pie -std=c++17 -o assign1.out main.o deliver.o
+g++ -m64 -no-pie -std=c++17 -o assignment1.out main.o deliver.o
 
-echo "Next ""assignment1 program"" will run"
+echo "Next assignment1 program will run"
 ./assignment1.out
-
- 
-
