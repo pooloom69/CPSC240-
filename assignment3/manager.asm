efb76d85639d8d0ebb065f6fa42874f11c686473
Sola Lhim <131292558+pooloom69@users.noreply.github.com>
2025-10-05 16:48:47 -0700

    Update manager.asm

--- assignment3/manager.asm
+++ assignment3/manager.asm
@@ -1,49 +1,41 @@
-;****************************************************************************************************************************
-; Project name: "Gnu Debugger".  
-; This program demonstrates how to make an assembly program that teaches all of the following:
-; This project develops a mixed-language program that integrates C/C++ functions with x86-64 assembly routines in order to 
-; create a workspace for GDB exploration. The program accepts an array of floating-point numbers from the user, 
-; computes their sum, sorts the values, and outputs the results.                                                          *
-; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-; version 3 as published by the Free Software Foundation.                                                                    *
-; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
-; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
-; A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
-;****************************************************************************************************************************
+// ****************************************************************************************************************************
+//  Project name: "Gnu Debugger".  
+//  This program demonstrates how to make an assembly program that teaches all of the following(Purpose):
+//  This project develops a mixed-language program that integrates C/C++ functions with x86-64 assembly routines in order to 
+//  create a workspace for GDB exploration. The program accepts an array of floating-point numbers from the user, 
+//  computes their sum, sorts the values, and outputs the results.                                                          *
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
+//  version 3 as published by the Free Software Foundation.                                                                    *
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+//  A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
+// ****************************************************************************************************************************
 
-; Author Information:
-; Author name    : Sola Lhim
-; Author email   : pooloom069@csu.fullerton.edu
-; Author CWID    : 830259727 
-;
-; Program information
-; Program name: manager.asm
-; Program languages: C86-64
-; Date Program began: 09/29/2025
-; Date of last update: //2025
-;		 
-; Purpose:
-; Coordinator between modules.
-; Calls inputarray to gather user’s numbers.
-; Calls sum to compute total.
-; Calls outputarray to print the entered numbers.
-; Calls sort (C/C++) to arrange numbers in ascending order.
-; Ensures the workflow follows the assignment diagram.
-;
-; Project Information:
-; The primary purpose of this project is to provide a structured environment for learning and practicing the GNU Debugger (GDB). 
-; While previous assignments focused on building functional x86-64 assembly and C/C++ programs, 
-; this assignment emphasizes the debugging process—setting breakpoints, stepping through code, examining registers and memory, 
-; and observing program behavior at runtime. By reusing and extending earlier functions (such as inputarray, outputarray, and sum), 
-; students can focus on mastering GDB commands and techniques in the context of a working program.
-;
-;
-; Files: driver.c, manager.asm, input_array.asm, output_array.asm, sum.as, swap.asm, sort.c data.inc, r.sh
-; Language: C86-64
-; Status: The program has been tested extensively with no detectable errors.
-;
-;
-;===== Begin code area ====================================================================================================================================================
+//  Author Information:
+//  Author name    : Sola Lhim
+//  Author email   : pooloom069@csu.fullerton.edu
+//  Author CWID    : 830259727 
+// 
+//  Program information
+//  Program name: Gnu Debugger
+//  Program languages: C86-64
+//  Date Program began: 09/29/2025
+//  Date of last update: 10//2025
+// 		 
+// 
+//  Project Information:
+//  The primary purpose of this project is to provide a structured environment for learning and practicing the GNU Debugger (GDB). 
+//  While previous assignments focused on building functional x86-64 assembly and C/C++ programs, 
+//  this assignment emphasizes the debugging process—setting breakpoints, stepping through code, examining registers and memory, 
+//  and observing program behavior at runtime. By reusing and extending earlier functions (such as inputarray, outputarray, and sum), 
+//  students can focus on mastering GDB commands and techniques in the context of a working program.
+// 
+// 
+//  Files: manager.asm
+//  Language: C86-64
+//  Status: The program has been tested extensively with no detectable errors.
+// 
+// ===== Begin code area ====================================================================================================================================================
 %include "data.inc"
 
 ; External functions 
@@ -78,8 +70,6 @@ curArray db "These are the current values in the array: ",10,0
 descTerminate db "This program will terminate",10,0
 descMessage db "Have a nice day %s.",10,0
 desc db "Invite a %s to come with you next time.",10,0
-
-
 
 
 
