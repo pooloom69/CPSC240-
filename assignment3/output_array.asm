6af2d93ba96ef255823df25fd5cca367f1ecee12
Sola Lhim <131292558+pooloom69@users.noreply.github.com>
2025-10-05 16:49:03 -0700

    Update output_array.asm

--- assignment3/output_array.asm
+++ assignment3/output_array.asm
@@ -1,44 +1,41 @@
-;****************************************************************************************************************************
-; Project name: "Gnu Debugger".  
-; This program demonstrates how to make an assembly program that teaches all of the following:
-; This project develops a mixed-language program that integrates C/C++ functions with x86-64 assembly routines in order to 
-; create a workspace for GDB exploration. The program accepts an array of floating-point numbers from the user, 
-; computes their sum, sorts the values, and outputs the results.                                                          *
-; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-; version 3 as published by the Free Software Foundation.                                                                    *
-; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
-; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
-; A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
-;****************************************************************************************************************************
+// ****************************************************************************************************************************
+//  Project name: "Gnu Debugger".  
+//  This program demonstrates how to make an assembly program that teaches all of the following(Purpose):
+//  This project develops a mixed-language program that integrates C/C++ functions with x86-64 assembly routines in order to 
+//  create a workspace for GDB exploration. The program accepts an array of floating-point numbers from the user, 
+//  computes their sum, sorts the values, and outputs the results.                                                          *
+//  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
+//  version 3 as published by the Free Software Foundation.                                                                    *
+//  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
+//  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+//  A copy of the GNU General Public License v3 is available here:  <https://www.gnu.org/licenses/>.                           *
+// ****************************************************************************************************************************
 
-; Author Information:
-; Author name    : Sola Lhim
-; Author email   : pooloom069@csu.fullerton.edu
-; Author CWID    : 830259727 
-;
-; Program information
-; Program name: output_array.asm
-; Program languages: C86-64
-; Date Program began: 09/29/2025
-; Date of last update: //2025
-;		 
-; Purpose:
-; Iterates through array values.
-; Prints them using library I/O (printf).
-;
-; Project Information:
-; The primary purpose of this project is to provide a structured environment for learning and practicing the GNU Debugger (GDB). 
-; While previous assignments focused on building functional x86-64 assembly and C/C++ programs, 
-; this assignment emphasizes the debugging process—setting breakpoints, stepping through code, examining registers and memory, 
-; and observing program behavior at runtime. By reusing and extending earlier functions (such as inputarray, outputarray, and sum), 
-; students can focus on mastering GDB commands and techniques in the context of a working program.
-;
-; Files: driver.c, manager.asm, input_array.asm, output_array.asm, sum.as, swap.asm, sort.c data.inc, r.sh
-; Language: C86-64
-; Status: The program has been tested extensively with no detectable errors.
-;
-;
-;===== Begin code area ====================================================================================================================================================
+//  Author Information:
+//  Author name    : Sola Lhim
+//  Author email   : pooloom069@csu.fullerton.edu
+//  Author CWID    : 830259727 
+// 
+//  Program information
+//  Program name: Gnu Debugger
+//  Program languages: C86-64
+//  Date Program began: 09/29/2025
+//  Date of last update: 10//2025
+// 		 
+// 
+//  Project Information:
+//  The primary purpose of this project is to provide a structured environment for learning and practicing the GNU Debugger (GDB). 
+//  While previous assignments focused on building functional x86-64 assembly and C/C++ programs, 
+//  this assignment emphasizes the debugging process—setting breakpoints, stepping through code, examining registers and memory, 
+//  and observing program behavior at runtime. By reusing and extending earlier functions (such as inputarray, outputarray, and sum), 
+//  students can focus on mastering GDB commands and techniques in the context of a working program.
+// 
+// 
+//  Files:  output_array.asm
+//  Language: C86-64
+//  Status: The program has been tested extensively with no detectable errors.
+// 
+// ===== Begin code area ====================================================================================================================================================
 %include "data.inc"
 
 
@@ -53,8 +50,6 @@ global output_array
 segment .data
 
 
-
-
 ; segment .bss is where uninitialized data is declared
 segment .bss
 
@@ -63,3 +58,31 @@ segment .text
 
 ; The header or label 'deliver' defines the initial program entry point
 output_array:
+
+; Macro backs up the GPRs
+backup
+
+
+; Save incoming double values
+mov r13, rdi    ; r13= array for float 
+mov r14, rsi    ; r14= size of array 
+mov r15, 0      ; r15= counts 
+
+
+; Check the array is empty
+cmp r14,0 
+call done
+
+
+
+
+
+
+
+
+
+
+; Restore original values to general registers
+done:
+restore 
+ret     
